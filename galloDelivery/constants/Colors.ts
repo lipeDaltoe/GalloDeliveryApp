7b732320bc3f1a78d61f26e8bd74ca71c6a694eb
lipeDaltoe <felipedaltoe9@gmail.com>
2025-08-25 07:58:25 -0300

    Criado o projeto

---
+++ galloDelivery/constants/Colors.ts
@@ -0,0 +1,25 @@
+const tintColorLight = '#2f95dc';
+const tintColorDark = '#fff';
+
+export default {
+    light: {
+        text: '#000000',
+        background: '#ffffff',
+        tint: tintColorLight,
+        tabIconDefault: '#cccccc',
+        tabIconSelected: tintColorLight
+    },
+    dark: {
+        text: '#ffffff',
+        background: '#000000',
+        tint: tintColorLight,
+        tabIconDefault: '#cccccc',
+        tabIconSelected: tintColorLight
+    },
+    primary: '#20E1B2',
+    lightGrey: '#FCF8FF',
+    grey: '#EEE9F0',
+    medium: '#9F9AA1',
+    mediumDark: '#424242',
+    green: '#437919',
+}
