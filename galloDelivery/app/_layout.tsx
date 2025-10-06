315174226b4822adf008e304ade5bda14f8f42a7
lipeDaltoe <felipedaltoe9@gmail.com>
2025-10-06 07:59:40 -0300

    criado modal filter

--- galloDelivery/app/_layout.tsx
+++ galloDelivery/app/_layout.tsx
@@ -2,6 +2,10 @@ import { Stack } from "expo-router";
 import CustomHeader from "@/components/CustomHeader";
 import { BottomSheetModalProvider } from '@gorhom/bottom-sheet';
 import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { Platform, TouchableOpacity } from 'react-native';
+import Colors from "@/constants/Colors";
+import Ionicons from "@expo/vector-icons/Ionicons";
+import { useRouter } from 'expo-router';
 
 export default function RootLayout() {
   return (
@@ -11,6 +15,26 @@ export default function RootLayout() {
           <Stack.Screen
             name="index"
             options={{ header: () => <CustomHeader /> }}
+          />
+          <Stack.Screen
+            name="(modal)/filter"
+            options={({ navigation }) => 
+              ({
+                 headerTitle: 'Filtro',
+                 headerTitleAlign: 'center',
+                 headerBackVisible: false,
+                 headerLeft: () => (
+                  <TouchableOpacity onPress={() => navigation.goBack()}>
+                    <Ionicons name="close-outline" size={30} color={Colors.primary} />
+                  </TouchableOpacity>
+                 ),
+                 headerStyle: {
+                    backgroundColor: Colors.lightGrey,
+                 },
+                 presentation: 'modal',
+                 animation: Platform.OS == 'ios' ? 'default' : 'slide_from_bottom',
+              })
+            }
           />
         </Stack>
       </BottomSheetModalProvider>
